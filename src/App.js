4ca40c859a01bcb98ad32eddd36c4903e56b84c4
MelodyZhangHelloWorld <ge.zhang01@edu.sait.ca>
2020-11-28 16:08:13 -0700

    noteStructures

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { Alert , Breadcrumb, Button, Card, Form, FormControl, Nav, Navbar } from 'react-bootstrap'; //*
+
+// import { Alert , Breadcrumb, Button, Card, Form, FormControl, Nav, Navbar } from 'react-bootstrap'; //*
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-    return ( < div className = "App" >
-        <header className = "App-header" >
-        
-        < img src = { logo }
-        className = "App-logo"
-        alt = "logo"  />
-        
-        <Button> Test Button </Button>
+import Todos from './components/Todos.js'
+import { Component } from 'react';
 
+class App extends Component {
 
-        </header> 
-        </div>
-    );
+    state ={
+        todos: [
+            {
+                id: 1,
+                title: 'Todo item1',
+                completed: false,
+            },
+            {
+                id: 2,
+                title: 'Todo item2',
+                completed: false,
+            },
+            {
+                id: 3,
+                title: 'Todo item3',
+                completed: true,
+            },
+            {
+                id: 4,
+                title: 'Todo item4',
+                completed: false,
+            },
+        ]
+
+    }
+
+render () {
+    
+  
+    console.log( this.state.todos)
+
+    return ( < div className = "App" >   
+    <header className = "App-header" >
+    
+   
+        <Todos 
+        todos={this.state.todos}
+        
+        />
+
+    </header> 
+    </div>
+);
+}
+   
 }
 
 export default App;
---
+++ src/components/Todos.js
@@ -0,0 +1,26 @@
+
+import React, { Component } from 'react'  //imc
+
+// function Todos() {
+//     return (
+//         <div className="Todos">
+//             <h1>Todos</h1>
+
+//         </div>
+//     );
+// }
+
+class Todos extends Component {
+    render() {
+   //     console.log("Todos.js -- props: ",this.props.todos)
+        return(
+            <div className="Todos">
+                  <h1>Todos</h1>
+
+            </div>
+        )
+    }
+}
+
+
+export default Todos;
